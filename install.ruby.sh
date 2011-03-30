3896a9cbafabc0fb80b5c857780e1e1a63507fa3
genokolar <genokolar@gmail.com>
2011-03-30 16:06:58 +0800

    first commit

---
+++ install.ruby.sh
@@ -0,0 +1,10 @@
+#sudo apt-get install build-essential gcc g++ libssl0.9.8 libssl0.9.8-dbg libssl-dev build-essential 
+sudo apt-get install ruby libzlib-ruby rdoc ri irb ruby1.8-dev  rubygems
+sudo gem update --system 
+#sudo gem install rails -y 
+#sudo gem install rails --include-dependencies 
+#sudo gem install mongrel -y 
+#sudo gem install mongrel_cluster --include-dependencies 
+#sudo apt-get install libopenssl-ruby libgd-ruby1.8 libyaml-ruby
+sudo apt-get install imagemagick libmagick9-dev
+sudo gem install rmagick -y
